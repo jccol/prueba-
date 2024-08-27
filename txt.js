2b91bad226fb0803c99036c74624c4ab3787feae
Jefferson Carranza <jeffersonc.sanchez07@gmail.com>
2024-08-27 13:30:24 -0500

    Primera version

---
+++ txt.js
@@ -0,0 +1,13 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+
+    let textoCifrado = texto.replace(/a/img, "ai")
+                            .replace(/e/img, "enter")
+                            .replace(/i/img, "imes")
+                            .replace(/o/img, "ober")
+                            .replace(/u/img, "ufat")
+}
+
+
+
+
